2cb9f63961bbfe0e55489b0eeffdda1a9cf97f4d
Oleksii Mosunov <alexmosunov@gmail.com>
2021-01-13 18:41:14 +0200

    Initial

--- Favourite Places app/ViewController.swift
+++ Favourite Places app/ViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let restaurants = ["restaurnat1","restaurnat2","restaurnat3",
+                       "restaurnat4","restaurnat5","restaurnat6",
+                       "restaurnat7","restaurnat8","restaurnat9",
+                       "restaurnat10","restaurnat11","restaurnat12",
+                       "restaurnat13","restaurnat14","restaurnat15",]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,3 +23,19 @@ class ViewController: UIViewController {
 
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurants.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = restaurants[indexPath.row]
+        cell.imageView?.image = UIImage(named: "pollo-vegetales2")
+        return cell
+        
+    }
+    
+    
+}
